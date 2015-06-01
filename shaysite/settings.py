7ca20f743707a1ef2578355233ace4aab96501f7
Shay-Jahen Merritté <shay@merritte.com>
2015-06-01 03:23:39 +0000

    linked static files

--- shaysite/settings.py
+++ shaysite/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MAIN_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'shaysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(MAIN_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,6 @@ TEMPLATE_DIRS = (
     os.path.join(MAIN_DIR, 'templates'),
     )
     
-STATIC_DIRS = (
+STATICFILES_DIRS = (
     os.path.join(MAIN_DIR, 'static'),
     )
--- shaysite/urls.py
+++ shaysite/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import patterns, include, url
+import core.views as coreviews
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-]
+urlpatterns = patterns('',
+    url(r'^admin/', coreviews.LandingView.as_view()),
+    (r'', include('core.urls')),
+)
